5394292e67b6a32a56eabd2af0a7168057071f3b
Johannes King <johannes.king@mpi-hd.mpg.de>
2015-11-17 20:31:29 +0100

    change

--- datasets/hess-crab4/simulate_eventlists.py
+++ datasets/hess-crab4/simulate_eventlists.py
@@ -5,69 +5,6 @@ import gammalib
 import ctools
 from astropy.io import fits
 
-def createobs(ra=83.633083, dec=22.014500, rad=5.0,
-              emin=0.1, emax=100.0, duration=360000.0, deadc=0.95,
-              ):
-    obs = gammalib.GCTAObservation()
-
-    # Set pointing direction
-    pntdir = gammalib.GSkyDir()
-    pntdir.radec_deg(ra, dec)
-    pnt = gammalib.GCTAPointing()
-    pnt.dir(pntdir)
-    obs.pointing(pnt)
-
-    # Set ROI
-    roi = gammalib.GCTARoi()
-    instdir = gammalib.GCTAInstDir()
-    instdir.dir(pntdir)
-    roi.centre(instdir)
-    roi.radius(rad)
-
-    # Set GTI
-    gti = gammalib.GGti()
-    start = gammalib.GTime(0.0)
-    stop = gammalib.GTime(duration)
-    gti.append(start, stop)
-
-    # Set energy boundaries
-    ebounds = gammalib.GEbounds()
-    e_min = gammalib.GEnergy()
-    e_max = gammalib.GEnergy()
-    e_min.TeV(emin)
-    e_max.TeV(emax)
-    ebounds.append(e_min, e_max)
-
-    # Allocate event list
-    events = gammalib.GCTAEventList()
-    events.roi(roi)
-    events.gti(gti)
-    events.ebounds(ebounds)
-    obs.events(events)
-
-    # Set ontime, livetime, and deadtime correction factor
-    obs.ontime(duration)
-    obs.livetime(duration * deadc)
-    obs.deadc(deadc)
-
-    # Return observation
-    return obs
-
-
-def make_dummy_eventlist():
-
-    filenames = [
-        'hess_events_023523_empty.fits',
-        'hess_events_023526_empty.fits',
-        'hess_events_023559_empty.fits',
-        'hess_events_023592_empty.fits',
-    ]
-
-    for f in filenames:
-        obs = createobs()
-        print('Writing {}'.format(f))
-        obs.save(f, True)
-
 
 def run_obssim():
     sim = ctools.ctobssim()
