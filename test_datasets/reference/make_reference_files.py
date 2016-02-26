428cad178fea4fbbc4bb935229df2e5b2704d0d2
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-02-26 15:37:26 +0100

    update reference files

--- test_datasets/reference/make_reference_files.py
+++ test_datasets/reference/make_reference_files.py
@@ -9,23 +9,24 @@ from gammapy.utils.scripts import make_path
 
 test_args = list()
 test_args.append(
-    dict(chain = 'HAP-HD',
-         store = 'hess-crab4-hd-hap-prod2',
-         obs = 23523,
-         aeff2D_reference_file = '$GAMMAPY_EXTRA/test_datasets/reference/hap_hd_aeff2D_reference.txt')
+    dict(chain='HAP-HD',
+         store='hess-crab4-hd-hap-prod2',
+         obs=23523,
+         aeff2D_reference_file='$GAMMAPY_EXTRA/test_datasets/reference/hap_hd_aeff2D_reference.txt',
+         edisp2D_reference_file='$GAMMAPY_EXTRA/test_datasets/reference/hap_hd_edisp2D_reference.txt',
+         psf3gauss_reference_file='$GAMMAPY_EXTRA/test_datasets/reference/hap_hd_psf3gauss_reference.txt')
 )
 test_args.append(
-    dict(chain = 'ParisAnalysis',
-         store = 'hess-crab4-pa',
-         obs = 23523,
-         aeff2D_reference_file = '$GAMMAPY_EXTRA/test_datasets/reference/pa_aeff2D_reference.txt')
+    dict(chain='ParisAnalysis',
+         store='hess-crab4-pa',
+         obs=23523,
+         aeff2D_reference_file='$GAMMAPY_EXTRA/test_datasets/reference/pa_aeff2D_reference.txt',
+         edisp2D_reference_file='$GAMMAPY_EXTRA/test_datasets/reference/pa_edisp2D_reference.txt',
+         psf3gauss_reference_file='$GAMMAPY_EXTRA/test_datasets/reference/pa_psf3gauss_reference.txt')
 )
 
-
 with open('reference_info.yaml', 'w') as outfile:
-    outfile.write(yaml.dump(test_args, default_flow_style=False) )
-
-
+    outfile.write(yaml.dump(test_args, default_flow_style=False))
 
 dm = data_manager()
 for chain in test_args:
@@ -34,3 +35,13 @@ for chain in test_args:
     filename = make_path(chain['aeff2D_reference_file'])
     f = open(str(filename), 'w')
     f.write(aeff.info())
+
+    edisp = store.load(chain['obs'], filetype='edisp')
+    filename = make_path(chain['edisp2D_reference_file'])
+    f = open(str(filename), 'w')
+    f.write(edisp.info())
+
+    psf3g = store.load(chain['obs'], filetype='psf')
+    filename = make_path(chain['psf3gauss_reference_file'])
+    f = open(str(filename), 'w')
+    f.write(psf3g.info())
