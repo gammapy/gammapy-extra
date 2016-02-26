125f986d009b0ddb272fbcd713735257c7a861b6
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-02-26 11:35:36 +0100

    update IRF reference files

--- test_datasets/reference/make_reference_files.py
+++ test_datasets/reference/make_reference_files.py
@@ -7,15 +7,17 @@ import yaml
 from gammapy.utils.testing import data_manager
 from gammapy.utils.scripts import make_path
 
-test_args = dict(test_run = 23523, chains = list())
-test_args['chains'].append(
+test_args = list()
+test_args.append(
     dict(chain = 'HAP-HD',
          store = 'hess-crab4-hd-hap-prod2',
+         obs = 23523,
          aeff2D_reference_file = '$GAMMAPY_EXTRA/test_datasets/reference/hap_hd_aeff2D_reference.txt')
 )
-test_args['chains'].append(
+test_args.append(
     dict(chain = 'ParisAnalysis',
          store = 'hess-crab4-pa',
+         obs = 23523,
          aeff2D_reference_file = '$GAMMAPY_EXTRA/test_datasets/reference/pa_aeff2D_reference.txt')
 )
 
@@ -26,9 +28,9 @@ with open('reference_info.yaml', 'w') as outfile:
 
 
 dm = data_manager()
-for chain in test_args['chains']:
+for chain in test_args:
     store = dm[chain['store']]
-    aeff = store.load(test_args['test_run'], filetype='aeff')
+    aeff = store.load(chain['obs'], filetype='aeff')
     filename = make_path(chain['aeff2D_reference_file'])
     f = open(str(filename), 'w')
     f.write(aeff.info())
