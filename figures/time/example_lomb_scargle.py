b40bf74c71391d0687f6723476a20efbc10d7e09
wegenmat <matthias.wegen@desy.de>
2017-09-12 22:58:29 +0200

    modified script to run from top repository

--- figures/time/example_lomb_scargle.py
+++ figures/time/example_lomb_scargle.py
@@ -1,9 +1,13 @@
+"""
+Plot example Lomb-Scargle figures for the Gammapy docs at
+http://docs.gammapy.org/en/latest/time/period.html
+"""
 import numpy as np
 from astropy.table import Table
 import sys
 
-from .....gammapy.gammapy.time.lomb_scargle import lomb_scargle
-from ....gammapy.gammapy.time.plot_periodogram import plot_periodogram
+from gammapy.time.lomb_scargle import lomb_scargle
+from gammapy.time.plot_periodogram import plot_periodogram
 
 table = Table.read('https://github.com/gammapy/gamma-cat/raw/master/input/data/2006/2006A%2526A...460..743A/tev-000119-lc.ecsv', format='ascii.ecsv')
 time = (table['time_max'].data + table['time_min'].data) / 2
@@ -11,7 +15,15 @@ flux = table['flux'].data
 flux_err = table['flux_err'].data
 
 result = lomb_scargle(time, flux, flux_err, 0.001, 10, 'boot')
-plot_periodogram(time, flux, flux_err, result['pgrid'],
-        result['psd'], result['swf'], result['period'],
-        result['significance']
-        )
+fig = plot_periodogram(time, flux, flux_err, result['pgrid'],
+                       result['psd'], result['swf'], result['period'],
+                       result['significance']
+                       )
+fig.savefig('short_test', bbox_inches='tight')
+
+result = lomb_scargle(time, flux, flux_err, 0.001, 'None', 'boot')
+fig = plot_periodogram(time, flux, flux_err, result['pgrid'],
+                       result['psd'], result['swf'], result['period'],
+                       result['significance']
+                       )
+fig.savefig('long_test', bbox_inches='tight')
