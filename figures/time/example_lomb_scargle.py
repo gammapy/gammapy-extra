fd44d38e68f41d9f214023a30de48955738a00ce
wegenmat <matthias.wegen@desy.de>
2017-09-15 13:26:06 +0200

    small improvements for print

--- figures/time/example_lomb_scargle.py
+++ figures/time/example_lomb_scargle.py
@@ -5,24 +5,32 @@ http://docs.gammapy.org/en/latest/time/period.html
 from astropy.table import Table
 from gammapy.time import lomb_scargle, plot_periodogram
 
+# get data
 url = 'https://github.com/gammapy/gamma-cat/raw/master/input/data/2006/2006A%2526A...460..743A/tev-000119-lc.ecsv'
 table = Table.read(url, format='ascii.ecsv')
 time = (table['time_max'].data + table['time_min'].data) / 2
 flux = table['flux'].data
 flux_err = table['flux_err'].data
 
-result = lomb_scargle(time, flux, flux_err, dt=0.001, max_period=10, criteria='boot')
+# set up parameters for Lomb-Scargle periodogram
+dt = 0.001
+max_period = 10
 
+# compute Lomb-Scargle periodogram
+result = lomb_scargle(time, flux, flux_err, dt=dt, max_period=max_period)
+
+# plot periodogram and save figure
 fig = plot_periodogram(
     time, flux, flux_err,
     periods=result['pgrid'],
     psd_data=result['psd'],
     psd_win=result['swf'],
     best_period=result['period'],
-    significance=result['fap'],
+    fap=result['fap'],
 )
-
-print(result['best_period'] + ' +/- ' + dt)
-print(result['fap'])
-
 fig.savefig('example_lomb_scargle.png', bbox_inches='tight')
+
+# print period of the highest periodogram peak and its false alarm probability
+print('Detected period: {} +/- {}'.format(result['period'], dt))
+print('False alarm probability of period:')
+print(result['fap'])
