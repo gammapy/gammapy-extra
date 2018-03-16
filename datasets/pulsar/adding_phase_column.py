db4f83b85049ac54310f99648278f8f24d24eb78
Christoph Deil <Deil.Christoph@gmail.com>
2018-03-16 11:04:09 +0100

    Small code cleanup in adding_phase_column.py

--- datasets/pulsar/adding_phase_column.py
+++ datasets/pulsar/adding_phase_column.py
@@ -1,20 +1,17 @@
-import os, sys
-from gammapy.data import DataStore
-from astropy.coordinates import SkyCoord
-import astropy.units as u
+"""Add phase for two obs on Vela pulsar from CTA DC-1"""
 import numpy as np
-from astropy.time import *
-
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord
+from gammapy.data import DataStore
 
 ###########################################################################
 #######################  Extracting Vela files  ###########################
 ###########################################################################
 
-# Set a path to the DC1 folder
-path = '/Users/mjacob/These/Pythonneries/CTA-DC1/1dc'
-
 # Load the data store (contains the information about all the DC1 data)
-data_store = DataStore.from_dir(path+'/index/gps')
+# Assumes you have an environment variable CTADATA set pointing to the DC1 folder
+data_store = DataStore.from_dir('$CTADATA/index/gps')
 table = data_store.obs_table
 
 # Defining the offset as the angular separation between the observation position and the target position
@@ -22,15 +19,18 @@ pos_obs = SkyCoord(table['GLON_PNT'], table['GLAT_PNT'], frame='galactic', unit=
 pos_target = SkyCoord.from_name('vela')
 offset = pos_target.separation(pos_obs)
 
-# Defining a mask to select all runs targetting Vela with an offset < 2 deg
+# Defining a mask to select all runs targeting Vela with an offset < 2 deg
 mask = (offset < 2 * u.deg)
+
 
 # Applying the mask
 table = table[mask]
 
 # Getting the list of observation indices of Vela
-id_obs_vela = table['OBS_ID'].data
+obs_ids = table['OBS_ID']
 
+# Here we just keep the first two observations
+obs_ids = obs_ids[:2]
 
 ###########################################################################
 ########################  Phasing the times  ##############################
@@ -64,18 +64,18 @@ def time2phase(t, t0, f0, f1, f2, phi_0=0):
 
 
 # Parameters for the Vela pulsar model for the DC1 found in model_galactic_pulsars.xml
-
 f0_vela = 11.19 * u.Hz
 f1_vela = -1.55e-11 * u.Hz / u.s
-f2_vela = 6.46e-23 * u.Hz / ((u.s)**2)
+f2_vela = 6.46e-23 * u.Hz / ((u.s) ** 2)
 t0_vela = Time(54686.2, format='mjd', scale='utc')
 
 # Loop over all Vela observations
-
-for obs_id in id_obs_vela:
-    obs_vela = data_store.obs(obs_id)
-    events_vela = obs_vela.events
-    times = events_vela.time  # Getting the times
+for obs_id in obs_ids:
+    events = data_store.obs(obs_id).events
+    times = events.time
     phases = np.fromiter((time2phase(times, t0_vela, f0_vela, f1_vela, f2_vela)), np.float)
-    obs_vela.events.table['PHASE VELA'] = phases
-    obs_vela.events.table.write("gps_baseline_{}.fits".format(obs_id))
+    events.table['PHASE'] = phases
+
+    filename = "gps_baseline_{:06d}.fits".format(obs_id)
+    print('Writing {}'.format(filename))
+    events.table.write(filename, overwrite=True)
