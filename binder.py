87192c2072b4b29dd7c695b9427bffee834cde41
Jose Enrique Ruiz <jer@iaa.es>
2018-07-27 11:07:33 +0200

    Fix pip upgrade in binder
    Upgrade of pip throws bug when installing gammapy with pip in the virtual env
    There is a comprehensive discussion about main function in pip upgrade here: https://github.com/pypa/pip/issues/5240

--- binder.py
+++ binder.py
@@ -2,10 +2,11 @@
 It installs software dependencies declared in environment.yml
 in the docker container built for the Binder service.
 """
-import pip
+
 import yaml
 import sys
 import conda.cli
+from pip._internal import main as pip_main
 
 with open("environment.yml") as stream:
     content = yaml.load(stream)
@@ -20,4 +21,4 @@ for pack in content['dependencies']:
         conda.cli.main('conda', 'install',  '-y', '-q', pack)
     else:
         print("RUN pip install {}".format(pack['pip'][0]))
-        pip.main(["install", pack['pip'][0]])
+        pip_main(["install", pack['pip'][0]])
