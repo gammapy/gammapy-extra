a73c9c617c10aa8983a9d679c92242c83d493c60
adonath <axel.donath@mpi-hd.mpg.de>
2018-08-07 14:38:19 +0200

    Update map cutout in notebooks

--- datasets/fermi_3fhl/make.py
+++ datasets/fermi_3fhl/make.py
@@ -3,9 +3,9 @@ from astropy.coordinates import SkyCoord, Angle
 from gammapy.maps import Map
 
 m = Map.read('gll_iem_v06.fits')
-m2 = m.make_cutout(
+m2 = m.cutout(
     SkyCoord(0, 0, unit='deg', frame='galactic'),
     (Angle('6 deg'), Angle('11 deg')),
-)[0]
+)
 print(m2.geom)
 m2.write('gll_iem_v06_cutout.fits', overwrite=True)
