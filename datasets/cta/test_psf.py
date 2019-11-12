66a928336c31b0791d8f648d363d6942cb398d0a
Christoph Deil <Deil.Christoph@gmail.com>
2019-11-12 14:57:47 +0100

    Remove old and unused datasets/cta/perf_prod2

--- datasets/cta/test_psf.py
+++
@@ -1,9 +0,0 @@
-from gammapy.irf import EnergyDependentMultiGaussPSF
-
-irf_path = 'perf_prod2/South_5h/irf_file.fits.gz'
-
-psf = EnergyDependentMultiGaussPSF.read(irf_path, hdu='POINT SPREAD FUNCTION')
-
-psf.peek()
-
-
