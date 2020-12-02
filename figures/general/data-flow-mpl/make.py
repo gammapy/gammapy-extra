fd2cb0b52a74f3f557a62275b33195dded4099c4
Axel Donath <axel.donath@mpi-hd.mpg.de>
2020-12-02 20:11:38 +0100

    Update

--- figures/general/data-flow-mpl/make.py
+++ figures/general/data-flow-mpl/make.py
@@ -18,24 +18,25 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 # Gammapy logo font
-matplotlib.rc('text', usetex=True)
-matplotlib.rc('font', **{'family': "sans-serif"})
-params = {'text.latex.preamble': [r'\usepackage{amsmath}']}
+matplotlib.rc("text", usetex=True)
+matplotlib.rc("font", **{"family": "sans-serif"})
+params = {"text.latex.preamble": [r"\usepackage{amsmath}"]}
 plt.rcParams.update(params)
 
 
 FIGSIZE_MM = [180, 111] * u.mm
 FIGSIZE = FIGSIZE_MM.to_value("inch")
 
-FOLDER_ICON = np.array([(0, 0), (0, 0.8), (0.08, 1), (0.5, 1), (0.58, 0.8), (0.93, 0.8), (0.93, 0)])
+FOLDER_ICON = np.array(
+    [(0, 0), (0, 0.8), (0.08, 1), (0.5, 1), (0.58, 0.8), (0.93, 0.8), (0.93, 0)]
+)
 
 
 def axis_to_fig(axis):
     fig = axis.figure
 
     def transform(coord):
-        return fig.transFigure.inverted().transform(
-            axis.transData.transform(coord))
+        return fig.transFigure.inverted().transform(axis.transData.transform(coord))
 
     return transform
 
@@ -45,16 +46,27 @@ def add_sub_axes(axis, rect):
     left, bottom, width, height = rect
     trans = axis_to_fig(axis)
     figleft, figbottom = trans((left, bottom))
-    figwidth, figheight = trans([width,height]) - trans([0,0])
+    figwidth, figheight = trans([width, height]) - trans([0, 0])
     return fig.add_axes([figleft, figbottom, figwidth, figheight])
 
 
-def plot_sub_package_icon(ax, offset=(0.5, 0.5), name=".makers", size=(22, 14), color="#3D3D3D", classes=[]):
-    p = Polygon(offset + size * FOLDER_ICON, fc="None", ec=color, lw=1, transform=ax.transData)
+def plot_sub_package_icon(
+    ax, offset=(0.5, 0.5), name=".makers", size=(22, 14), color="#3D3D3D", classes=[]
+):
+    p = Polygon(
+        offset + size * FOLDER_ICON, fc="None", ec=color, lw=1, transform=ax.transData
+    )
     ax.add_artist(p)
 
     fontsize_gp = 32
-    ax.text(offset[0] + 1, offset[1] + 5, s="$\gamma$", size=fontsize_gp, va="bottom", color="#FC3617")
+    ax.text(
+        offset[0] + 1,
+        offset[1] + 5,
+        s="$\gamma$",
+        size=fontsize_gp,
+        va="bottom",
+        color="#FC3617",
+    )
     ax.text(offset[0] + 6, offset[1] + 5, s="$\pi$", size=fontsize_gp, color=color)
     ax.text(offset[0] + 1, offset[1] + 1, s=name, size=12, color=color)
 
@@ -65,10 +77,10 @@ def plot_sub_package_icon(ax, offset=(0.5, 0.5), name=".makers", size=(22, 14), 
 def plot_brace(ax, x, y, scale):
     tp = TextPath((0, 0), "}", size=1)
     trans = (
-            mtrans.Affine2D().scale(1, scale)
-            + mtrans.Affine2D().rotate(-90)
-            + mtrans.Affine2D().translate(x, y)
-            + ax.transData
+        mtrans.Affine2D().scale(1, scale)
+        + mtrans.Affine2D().rotate(-90)
+        + mtrans.Affine2D().translate(x, y)
+        + ax.transData
     )
     pp = PathPatch(tp, lw=1, fc="k", transform=trans)
     ax.add_artist(pp)
@@ -82,7 +94,9 @@ def plot_arrow(ax, offset, dx=10, dy=0, **kwargs):
     kwargs.setdefault("length_includes_head", True)
     kwargs.setdefault("width", 1)
 
-    arrow = FancyArrow(offset[0], offset[1], dx=dx, dy=dy, transform=ax.transData, **kwargs)
+    arrow = FancyArrow(
+        offset[0], offset[1], dx=dx, dy=dy, transform=ax.transData, **kwargs
+    )
     ax.add_artist(arrow)
 
 
@@ -131,8 +145,12 @@ def plot_sed(ax):
     flux_points = FluxPoints.read(filename)
     flux_points.table["is_ul"] = flux_points.table["ts"] < 4
     flux_points.plot(
-        ax=ax, energy_power=2, flux_unit="erg-1 cm-2 s-1", color="darkorange", elinewidth=1,
-        markeredgewidth=1
+        ax=ax,
+        energy_power=2,
+        flux_unit="erg-1 cm-2 s-1",
+        color="darkorange",
+        elinewidth=1,
+        markeredgewidth=1,
     )
 
     flux_points.to_sed_type("e2dnde").plot_ts_profiles(ax=ax, add_cbar=False)
@@ -145,7 +163,7 @@ def plot_image(ax):
     log.info(f"Reading: {filename}")
     m = Map.read(filename)
     m.plot(ax=ax, cmap="inferno", stretch="sqrt")
-    ax.set_title("Flux maps")
+    ax.set_title("Flux maps", color="#3D3D3D")
     format_dl5_ax(ax=ax)
 
 
@@ -162,7 +180,7 @@ def main(draft=True):
     ax.xaxis.set_minor_locator(MultipleLocator(10))
     ax.yaxis.set_minor_locator(MultipleLocator(10))
 
-    classes = ["DataStore", "Observations", "Observation"]
+    classes = ["DataStore", "Observations", "Observation", "GTI"]
     plot_sub_package_icon(ax, offset=(5, 50), name=".data", classes=classes)
 
     classes = [
@@ -170,37 +188,115 @@ def main(draft=True):
         "SafeMaskMaker",
         "FoVBackgroundMaker",
         "RingBackgroundMaker",
-        "etc."
+        "etc.",
     ]
-    plot_sub_package_icon(ax, offset=(37.5, 50), name=".makers", color="gray", classes=classes)
+    plot_sub_package_icon(
+        ax, offset=(37.5, 50), name=".makers", color="gray", classes=classes
+    )
 
-    classes = ["Datasets", "MapDataset", "MapDatasetOnOff"]
+    classes = ["Datasets", "MapDataset", "MapDatasetOnOff", "etc."]
     plot_sub_package_icon(ax, offset=(70, 50), name=".datasets", classes=classes)
 
-    classes = ["FluxPointsEstimator", "FluxMapEstimator"]
-    plot_sub_package_icon(ax, offset=(105, 35), name=".estimators", color="gray", classes=classes)
+    classes = ["FluxPointsEstimator", "FluxMapEstimator", "etc."]
+    plot_sub_package_icon(
+        ax, offset=(105, 35), name=".estimators", color="gray", classes=classes
+    )
 
-    classes = ["Fit", "Models", "SkyModel", "FoVBackgroundModel"]
-    plot_sub_package_icon(ax, offset=(105, 75), name=".modeling", color="gray", classes=classes)
+    classes = ["Fit", "Models", "SkyModel", "FoVBackgroundModel", "etc."]
+    plot_sub_package_icon(
+        ax, offset=(105, 78), name=".modeling", color="gray", classes=classes
+    )
 
     plot_sub_package_icon(ax, offset=(55, 2), name=".analysis")
 
     # data levels
     color = "#3D3D3D"
     ypos = 105
-    ax.text(15, ypos, "DL3", size=24, transform=ax.transData, va="center", ha="center", color=color)
-    ax.text(80, ypos, "DL4", size=24, transform=ax.transData, va="center", ha="center", color=color)
-    ax.text(160, ypos, "DL5", size=24, transform=ax.transData, va="center", ha="center", color=color)
+    ax.text(
+        15,
+        ypos,
+        "DL3",
+        size=24,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+    )
+    ax.text(
+        80,
+        ypos,
+        "DL4",
+        size=24,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+    )
+    ax.text(
+        160,
+        ypos,
+        "DL5",
+        size=24,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+    )
     plot_arrow(ax, offset=(27.5, ypos), dx=40, fc="gray")
     plot_arrow(ax, offset=(92.5, ypos), dx=55, fc="gray")
 
     ypos = 100
-    ax.text(47.5, ypos, "Data reduction", size=12, transform=ax.transData, va="center", ha="center", color="gray")
-    ax.text(117.5, ypos, "MLE fitting", size=12, transform=ax.transData, va="center", ha="center", color="gray")
+    ax.text(
+        47.5,
+        ypos,
+        "Data reduction",
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color="gray",
+    )
+    ax.text(
+        117.5,
+        ypos,
+        "MLE fitting",
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color="gray",
+    )
 
-    ax.text(15, ypos, "$\gamma$-like events", size=12, transform=ax.transData, va="center", ha="center", color=color)
-    ax.text(80, ypos, "Binned datasets ", size=12, transform=ax.transData, va="center", ha="center", color=color)
-    ax.text(160, ypos, "Science products", size=12, transform=ax.transData, va="center", ha="center", color=color)
+    ax.text(
+        15,
+        ypos,
+        "$\gamma$-like events",
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+    )
+    ax.text(
+        80,
+        ypos,
+        "Binned datasets ",
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+    )
+    ax.text(
+        160,
+        ypos,
+        "Science products",
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+    )
 
     plot_arrow(ax, offset=(27, 56), fc="gray")
     plot_arrow(ax, offset=(59, 56))
@@ -208,9 +304,28 @@ def main(draft=True):
     plot_curved_arrow(ax, posA=(91, 56), posB=(105, 82))
 
     connectionstyle = "bar,angle=-90,fraction=-0.335"
-    plot_curved_arrow(ax, posA=(91, 56), posB=(105, 42), connectionstyle=connectionstyle)
+    plot_curved_arrow(
+        ax, posA=(91, 56), posB=(105, 42), connectionstyle=connectionstyle
+    )
 
-    ax.text(95, 72, "join or stack", rotation=90, size=12, transform=ax.transData, va="center", ha="center", color="gray")
+    ax.text(
+        95,
+        72,
+        "join or stack",
+        rotation=90,
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color="gray",
+    )
+
+    xpos = 138
+    plot_arrow(ax, offset=(xpos, 68), dx=6)
+    plot_arrow(ax, offset=(xpos, 40), dx=6)
+    plot_arrow(ax, offset=(xpos, 12), dx=6)
+    plt.plot([138, 138], [12, 68], lw=3, color=color)
+    plt.plot([128, 138], [40, 40], lw=3, color=color)
 
     if draft:
         plt.grid(alpha=0.2, lw=0.5)
@@ -226,9 +341,23 @@ def main(draft=True):
     ax_lc = add_sub_axes(ax, [145, 2, 30, 20])
     plot_lightcurve(ax=ax_lc)
 
-    p2 = (5, 25)
-    p1 = (125, 25)
-    curlyBrace(fig, ax, p1, p2, k_r=0.03, bool_auto=True, color=color, lw=2, int_line_num=1)
+    ax.text(
+        160,
+        90,
+        "Source Catalogs",
+        size=12,
+        transform=ax.transData,
+        va="center",
+        ha="center",
+        color=color,
+        weight="black",
+    )
+
+    p2 = (5, 23.5)
+    p1 = (128, 23.5)
+    curlyBrace(
+        fig, ax, p1, p2, k_r=0.025, bool_auto=True, color=color, lw=2, int_line_num=1
+    )
 
     filename = "overview.pdf"
     log.info(f"Writing {filename}")
